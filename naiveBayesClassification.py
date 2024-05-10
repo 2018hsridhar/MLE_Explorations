9438ad1e9a43b6eed2b77377a23d0dcc051dd324
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-05-09 17:43:33 -0700

    Update naiveBayesClassification.py
    Expanding out initial NLP understanding -> computing weightage metrics.

--- naiveBayesClassification.py
+++ naiveBayesClassification.py
@@ -3,13 +3,58 @@ Working with Python Jupyter Notebooks
 Goal : To Explore Supervised Learning Methods -> in this case, Naive Bayes Classification
 We predicate many SL methods off of the NB method
 
-Setting up Pythonic Naive Bayes Classification.
-Let's explore how to evolve and iterate on a classificier from two class ( binomial ) to multi class.
-We'll also explore the methods of TF ( Total Frequency ), IDF ( Inverse Document Frequency ), and TF-IDF ( which combines the two as a product ).
-Lastly, we'll also explore vectorization of counts based on a vocabulary of words, and representing non-mathematical inputs more mathematically.
+Links : 
+1. https://www.opinosis-analytics.com/knowledge-base/term-frequency-explained/#:~:text=Term%20frequency%20(TF)%20means%20how,about%20how%20you%20define%20it.
 
-Good canon case studies :
-1. UberEats Market Campaigns -> targeting users : can we make non-Habitual Users Habitual Users, following a promitions campaign? Do we have to promote to a massive user base, or a cohort group only?
 
 
 '''
+
+'''
+Normalization method : less systematic bias from longer documents.
+'''
+
+'''
+Better if can make in-place instead
+Extends by passing stopWords ( versus preDefList ) 
+
+'''
+
+def testNaturalLanguageProcessingMethods() -> bool:
+    nlpMethodsWork = True
+    stopWordsListOne = ['the','and','or','but', 'this', 'that']
+    stopWordsListTwo = ['a','at','is','are','to','be']
+    return nlpMethodsWork
+
+
+'''
+Make topic words ( not stop words ) appear more often
+
+'''
+def stopWordRemoval(stopWords:, doc:string) -> List[String]:
+    stopWordsRemovedDoc = []
+    for token in doc:
+        if token not in stopWords:
+    return stopWordsRemovedDoc
+
+'''
+'''
+def computeInverseDocumentFrequency(targetTerm: string, docSet:[string]) -> int:
+    idf = 0
+    for doc in docSet:
+    
+    return idf
+
+'''
+TF : scoped down to a single document
+'''
+def computeTermFrequency(targetTerm:string, doc:string) -> int:
+    delimeter = "\\s+"
+    docTokens = doc.split(delimeter)
+    totalNumberDocTerms = len(docTokens)
+    targetTermFreq = 0
+    for token in docTokens:
+        if token == targetTerm:
+            targetTermFreq += 1
+    return (targetTermFreq / totalNumberDocTerms)
+
