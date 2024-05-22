c4564ee308deff2dcde8eb79cf5a54149272bd7e
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-05-21 17:51:36 -0700

    Update neuron.py
    Using commit changes as note taking mechanism 
    1. That gradient descent undergirds the learning and training taking place in ML algorithms.
    2. That grad descent is a first-order, iterative approach
    3. That gradient descent can be computed @ different levels : averaged over entire dataset, per example, or batched across examples. Three different levels of computation means three different behaviors for model convergence

--- neuron.py
+++ neuron.py
@@ -1,4 +1,13 @@
 '''
+NN Notes :
+- operate on non-linear data.
+- it is supervised learning
+- specializations needed on Perceptron?
+- (sigmoid(sum(inputs*weights + bias)) = output)
+- sigmoid(weight*feature + bias ) = prediction
+
+
+
 No regularization ( huh )?
 Use sigmoid activation func.
 Why to use mini-batch gradient descent?
@@ -8,7 +17,15 @@ sigma : non-linear func
     sigma(summation) => output
 ( single bias, multiple features )
 
+Test scoping :
+a. does the model generalize well to non-training data?
+b.
+c.
+d.
+e.
 
+Resources :
+a. https://towardsdatascience.com/batch-mini-batch-stochastic-gradient-descent-7a62ecba642a
 
 '''
 import numpy as np
@@ -32,22 +49,51 @@ class Neuron:
     # Ahh shit a single neuron has to execute learning on the feature set too
     # Training should adjust our weights
     # Gradient(of loss function) -> gradient on an error ( ohh ) 
+
+
+    # A compromise between computing the true gradient and the gradient at a single sample
+    # is to compute the gradient against more than one training sample (called a "mini-batch") at each step.
+    # This can perform significantly better than "true" stochastic gradient descent described,
+    # because the code can make use of vectorization libraries rather than computing each
+    # step separately as was first shown in [6] where it was called
+    # "the bunch-mode back-propagation algorithm". It may also result in smoother convergence, as
+    # the gradient computed at each step is averaged over more training samples.
+    # Is mini-batch even the norm for NNs?
+    # Batch size of 10 : kept small for faster convergence
+    # randomize samples or not?
+
+    # Multiple epochs in training ( iterate over each epoch ) ; data fed in multiple times.
+    # Self-feed data into NN multiple times.
+    # time to epoch completion = ( data_set_size / batch_size )
+        
     def train(self, learning_rate=0.01, batch_size=10, epochs=200):
-
         # Set up sigmoid functionality
+        # [1] step one : compute sigmoid(transpose(weight)*feature) and losses for each example
         lossVals = []
-        for example in self.examples
-            y = example[-1]
-            curFeature = example[0:len(example) - 1:1]
-            neuronSum = 0
-            for i in len(curFeature):
-                neuronSum += curFeature[i] * self.weights[i]
-            yHat = self.sigmoid(neuronSum)
-            curLoss = self.lossFunc(yHat,y)
-            lossVec.append(curLoss)
+        datasetSize = len(self.examples)
+        for epoch in range(len(epochs)):
+            for(batchPtr in range(0, datasetSize, batch_size)):
+                # [1] Compute gradient on the batch size
+                
 
-        # NN stuff is well-predicated on logistic regression ( master this ) 
-
+                # [2] update all examples ( in the current batch ) accordingly
+                y = example[-1]
+                curFeature = example[0:len(example) - 1:1]
+                neuronSum = 0
+                for i in len(curFeature):
+                    neuronSum += curFeature[i] * self.weights[i]
+                biasIndex = 3
+                neuronSum += self.weights[biasIndex]
+                yHat = self.sigmoid(neuronSum)
+                curLoss = self.lossFunc(yHat,y)
+                lossVec.append(curLoss)
+            
+        
+            
+        # [3] update trainable params - the weights - with loss values
+        # for lossW in self.weights
+            # self.weights += learning_rate * lossW
+        
     # Negative log loss
     # Woah loss akin to that of logistic regression
     def lossFunc(self,inputVal):
@@ -55,13 +101,17 @@ class Neuron:
 
 
     # Initially, sigmoid and nn models similar ( with sigmoid )
+    # Solve for the exponential
+    # Does this really reduce training loss?
     def sigmoid(self,neuronSum):
-        # bias : a fixed constant?
-        sigmoid = func(neuronSum) + bias
+        denom = 1 + np.exp(-1 * neuronSum)
+        sigmoid = 1 / denom
         return sigmoid
-        
 
     # Return the probability—not the corresponding 0 or 1 label.
+    # input has label of <1> ( single label based probability )
     def predict(self, features):
         groundLabelProb = 0.0
+        
+        
         return groundLabelProb
