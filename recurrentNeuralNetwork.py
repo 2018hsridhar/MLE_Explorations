6759edcaf48a81f6070710baf348dd542543fe89
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-06-04 14:36:04 -0700

    Create recurrentNeuralNetwork.py
    So will we even get around to GRUs and LSTMs? What about ChatGPT?

---
+++ recurrentNeuralNetwork.py
@@ -0,0 +1,6 @@
+'''
+Goals :
+1. Code up a RNN : Recurrent Neural Network. This is a type of DNN ( Deep Neural Network )
+2. Find and import data that RNNs can work on : usually in the form of sequential ( if time ) or ordering ( no time ) 
+3. 
+'''
