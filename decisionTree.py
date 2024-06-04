a8d8d5b0179e9f0d0b05e70ff84be06074c0a2f9
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-06-04 13:52:01 -0700

    Create decisionTree.py
    Created an empty template for the simplest tree-based machine learning classifier : decision-trees. We'll be figuring out decision tree parameters and modeling as we go :-)

---
+++ decisionTree.py
@@ -0,0 +1,8 @@
+'''
+Overarching Goals :
+1. Set up a basic decision tree classifier : binary -> then multi-class.
+2. Explore decision tree modeling : number of levels, questions asked at each step, thresholds to set.
+3. Investing iteratively improving the decision tree structure.
+
+#TODO : Implement a basic decision tree
+'''
