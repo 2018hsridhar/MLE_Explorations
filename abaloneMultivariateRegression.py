156c3ae63858047d0a46afaaba813a0205972cae
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-06-05 18:27:09 -0700

    Update abaloneMultivariateRegression.py
    Learning to use numpy in order to apply transformations to featureLists and feature vectors as machine learning model inputs. 
    
    Note : multiple linear regression ( scalar(y)) != multivaraible linear regression ( nDvec(y))

--- abaloneMultivariateRegression.py
+++ abaloneMultivariateRegression.py
@@ -1,5 +1,6 @@
 '''
-Basic Multiple Regression Example
+Basic Multiple Linear Regression Example ( single scalar y ) 
+    Not yet multivariate linear regression.
 Import dataset as CSV from UCI Machine Learning
 
 Goal : Given Physical Measurements, Predict Age ( Number of Rings )
@@ -10,6 +11,7 @@ Dataset is labeled
 Key modules
 '''
 import pandas as pd
+import numpy as np
 from sklearn import linear_model
 
 # filepath = "C:\Users\haris\OneDrive\Desktop\ML-Datasets\abalone\abalone.csv"
@@ -25,22 +27,41 @@ sampleSize = len(df)
 numFeatures = len(df.columns) - 1
 # print("Sample Size = " + str(sampleSize))
 # print("Num features = " + str(numFeatures))
-labelIndex = numFeatures - 1
-X = df[[df.columns[featureIndex] for featureIndex in range(len(df.columns)) if featureIndex != labelIndex]]
-y = df.iloc[:, labelIndex]
 
 # print(X.head)
 # print(Y.head)
 
 # how to convert strings to floats here though?
-regr = linear_model.LinearRegression()
-regr.fit(X,y)
 
+# Apply dataframe transformations.
+# lambda function apply(...) change all values
+df.iloc[:,0]=df.iloc[:, 0].apply(lambda x:x.replace("M","0"))
+df.iloc[:,0]=df.iloc[:, 0].apply(lambda x:x.replace("F","1"))
+df.iloc[:,0]=df.iloc[:, 0].apply(lambda x:x.replace("I","2"))
+# print(df.head)
+# print(df.dtypes)
 
+# pd.to_numeric(df, errors='coerce')
+# print(df.dtypes)
+# pd.to_numeric(df['points'], downcast='float')
+df.iloc[:,0] = pd.to_numeric(df.iloc[:,0], downcast='float')
+# print(df.dtypes)
 
-featureListExampleOne = ["M", 0.455, 0.365, 0.095, 0.514, 0.2245, 0.101,0.15, 15]
-predictedNumAbaloneRings = regr.predict(featureListExampleOne)
-print(predictedNumAbaloneRings)
+# Now apply data fit to our ML models.
+labelIndex = numFeatures - 1
+X = df[[df.columns[featureIndex] for featureIndex in range(len(df.columns)) if featureIndex != labelIndex]]
+y = df.iloc[:, labelIndex]
+multivarRegr = linear_model.LinearRegression()
+multivarRegr.fit(X,y)
+
+print("Coefficients of multilinear regression are:")
+print(multivarRegr.coef_)
+
+featureListExampleOne = [1, 0.455, 0.365, 0.095, 0.514, 0.2245, 0.101,0.15]
+# featureListExampleOne.reshape(-1, 1)
+reshapedFeature = np.array(featureListExampleOne).reshape(1,-1)
+predictedNumAbaloneRings = multivarRegr.predict(reshapedFeature)
+print("For the given feature vector, my number of predicted abolone rings = " + str(predictedNumAbaloneRings))
 
 
 
