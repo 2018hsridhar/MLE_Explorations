50e82efe38143b649eebf7e43706325ab97cee88
Hari Sridhar <hari.h.sridhar@gmail.com>
2025-08-29 14:45:50 -0700

    Reading and plotting kaggle data

--- SUPERVISED_LEARNING/LINEAR_REGRESSION/UTILS/DatasetLoader.py
+++ SUPERVISED_LEARNING/LINEAR_REGRESSION/UTILS/DatasetLoader.py
@@ -6,16 +6,17 @@ class DatasetLoader:
     def __init__(self):
         self.api = KaggleApi()
         self.api.authenticate()
-        self.DELIM = ';'
+        self.COMMA = ','
 
     def getDataset(self, target_path, target_dataset) -> pd.DataFrame:
         self.api.dataset_download_files(target_dataset, path=target_path, unzip=True)
-        df = pd.read_csv(f'{target_path}/powerconsumption.csv', sep=self.DELIM)
+        df = pd.read_csv(f'{target_path}/powerconsumption.csv', sep=self.COMMA)
         return df
 
     def print_dataset_summary_statistics(self,target_dataset,target_path,df) -> None:
         print("DATASET SUMMARY STATISTICS")
         print(f"Dataset name {target_dataset} loaded from Kaggle from targetPath {target_path}")
+        print(f"Dimensions are : {df.shape}")
         print(f"Number of Rows: {df.shape[0]:,}")
         print(f"Number of Columns: {df.shape[1]}")
         print(f"Column names: {df.columns.tolist()}")
