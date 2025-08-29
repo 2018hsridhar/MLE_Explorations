770215c5dba61621dd2dda0112f8983137411edd
Hari Sridhar <hari.h.sridhar@gmail.com>
2025-08-29 13:19:05 -0700

    Reorder

---
+++ SUPERVISED_LEARNING/DECISION_TREES/randomForest.py
@@ -0,0 +1,10 @@
+'''
+
+Outcome Goals :
+1. Improve iteratively over a single decision tree -> leverage majority voting across a set of decision trees - d1,...,dn - with elemetns of randomness.
+2. Hypothesis : Randomness and majority should lead to more accurate predictions.
+3. Evaluate random forest against other classification models ( especially single decisoin tree ) and check if it's more performant.
+4. Explore random forest parameter space, encompassing the following : number of trees, majority ratio-proportion, intensity of introduced randomness.
+
+
+'''
