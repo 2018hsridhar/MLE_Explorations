b0e063cac434091752495fb746ed0cb500624145
Hari Sridhar <hari.h.sridhar@gmail.com>
2025-08-29 22:19:45 -0700

    No module named kagle err

--- SUPERVISED_LEARNING/LINEAR_REGRESSION/twoDimLinearRegression.py
+++ SUPERVISED_LEARNING/LINEAR_REGRESSION/twoDimLinearRegression.py
@@ -16,6 +16,7 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..','..'))
 
 # Now use absolute imports
 from UTILS.DatasetLoader import DatasetLoader
