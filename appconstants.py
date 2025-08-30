bd01bd0a0d2677463772f64b3067ce93d1d0118d
Hari Sridhar <hari.h.sridhar@gmail.com>
2025-08-29 22:05:26 -0700

    ImageLoader Class for CNNs in the making

---
+++ appconstants.py
@@ -0,0 +1,8 @@
+# Example datasets you can use:
+IMAGE_DATASETS = {
+    'cats_vs_dogs': 'tongpinmo/cat-and-dog',
+    'cifar10': 'jidhumohan/cifar10-dataset', 
+    'flowers': 'alxmamaev/flowers-recognition',
+    'food101': 'dansbecker/food-101',
+    'animal_faces': 'andrewmvd/animal-faces'
+}
